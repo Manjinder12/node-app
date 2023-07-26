5559e1fcdb00825b9d01e3d6b2037eb5698d93a4
Manjinder Singh <manjinder.singh@insurancesamadhan.com>
2023-07-26 17:15:19 +0530

    jenkins test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World,hello test,new test\n");
+  response.end("Hello World,hello test,new test,jenkins test\n");
 });
 server.listen(9095);
 console.log("Server running at http://127.0.0.1:9095/");
