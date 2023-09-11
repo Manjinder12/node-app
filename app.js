2bf43e5d95a87f277de29d259d73f2bed2337576
DevOps Engineer <apple@apples-MacBook-Air.local>
2023-09-11 10:46:11 +0530

    new script test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World,testAgain,pipeline,demo\n");
+  response.end("Hello World,pipeline,demo\n");
 });
 server.listen(9095);
 console.log("Server running at http://127.0.0.1:9095/");
