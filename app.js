a29ef99885a5502d3ad7240087565808180dfe04
DevOps Engineer <apple@apples-MacBook-Air.local>
2023-09-13 15:16:55 +0530

    rollback test2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World,hello test,demo\n");
+  response.end("Hello World,hello test, Rollback, demo\n");
 });
 server.listen(9095);
 console.log("Server running at http://127.0.0.1:9095/");
