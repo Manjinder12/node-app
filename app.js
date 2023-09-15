7c90dbb7127f1c694c446d93ee04cc3baaee52ba
Manjinder Singh <srandhawa34@gmail.com>
2023-09-15 14:51:27 +0530

    change code

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-var http = require('http');
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World,hello test, Rollback, demo\n");
+var express  =  require('express');
+var app = express();
+const port = 9095;
+
+app.get('/', (req, res) =>{
+console.log('Inside / route');	
+res.send('APP WORKING!!!')
+})
+
+app.listen(port, () => {
+	console.log(`App Is Working On ${port}`)
 });
-server.listen(9095);
-app.use('/',fuction(){console.log('app is working')})
-console.log("Server running at http://127.0.0.1:9095/");
