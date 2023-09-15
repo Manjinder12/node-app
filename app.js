a6bf2543fb463e0f20ae237d8787170b76186f62
Manjinder Singh <srandhawa34@gmail.com>
2023-09-15 14:35:43 +0530

    added logs file

--- app.js
+++ app.js
@@ -4,4 +4,5 @@ var server = http.createServer(function (request, response) {
   response.end("Hello World,hello test, Rollback, demo\n");
 });
 server.listen(9095);
+app.use('/',fuction(){console.log('app is working')})
 console.log("Server running at http://127.0.0.1:9095/");
